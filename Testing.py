0802ca77aa5f118015dc0deaffbf75474adb153d
UnicycleUnicorn <brand227796@gmail.com>
2023-05-04 12:23:57 -0400

    Key generation attempt 2a

--- Testing.py
+++ Testing.py
@@ -1,26 +1,8 @@
 from CryptWrapper import CryptWrapper
-import time
 
-a = time.time_ns()
-s_priv, s_pub = CryptWrapper.generate_dh_keys()
-b = time.time_ns()
-o_priv, o_pub = CryptWrapper.generate_dh_keys()
-c = time.time_ns()
-s_aes_gcm = CryptWrapper.generate_aes_gcm_key(s_priv, o_pub)
-d = time.time_ns()
-o_aes_gcm = CryptWrapper.generate_aes_gcm_key(o_priv, s_pub)
-e = time.time_ns()
+dh_private, dh_public = CryptWrapper.generate_dh_keys()
+print(dh_public)
+other_dh_public = input("Enter other public: ")
 
-print(s_pub)
-print(o_pub)
-
-message = b'Hello World!'
-print(message)
-ee = time.time_ns()
-ciphertext = CryptWrapper.encrypt(s_aes_gcm, message)
-f = time.time_ns()
-print(ciphertext)
-ff = time.time_ns()
-plaintext = CryptWrapper.decrypt(o_aes_gcm, ciphertext)
-g = time.time_ns()
-print(plaintext)
+s_aes_gcm = CryptWrapper.generate_aes_gcm_key(dh_private, other_dh_public.encode())
+print("AES GCM KEYS GENERATED")
